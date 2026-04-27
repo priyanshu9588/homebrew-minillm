30c5128738d87494c63eab62933575e1638bfa34
Priyanshu. <priyanshujhawar3@gmail.com>
2026-04-27 23:57:49 +0530

    Fresh start

---
+++ Casks/minillm.rb
@@ -0,0 +1,29 @@
+cask "minillm" do
+  version "0.0.0"
+  sha256 "NO_RELEASE_YET"
+
+  url "https://github.com/priyanshu9588/homebrew-minillm/releases/download/v#{version}/MiniLLM_#{version}_aarch64.dmg",
+      verified: "github.com/priyanshu9588/homebrew-minillm/"
+  name "MiniLLM"
+  desc "Private on-device LLM chat powered by Gemma 4"
+  homepage "https://github.com/priyanshu9588/minillm"
+
+  depends_on arch: :arm64
+  depends_on macos: ">= :monterey"
+
+  app "MiniLLM.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-cr", "#{appdir}/MiniLLM.app"],
+                   sudo: false
+  end
+
+  zap trash: [
+    "~/Library/Application Support/dev.priyanshu.minillm",
+    "~/Library/Caches/dev.priyanshu.minillm",
+    "~/Library/Preferences/dev.priyanshu.minillm.plist",
+    "~/Library/Saved Application State/dev.priyanshu.minillm.savedState",
+    "~/Library/WebKit/dev.priyanshu.minillm",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,8 @@
+# homebrew-minillm
+
+Homebrew tap for [MiniLLM](https://github.com/priyanshu9588/minillm).
+
+```sh
+brew tap priyanshu9588/minillm
+brew install --cask minillm
+```
